eb706f73e196bbc8895f881179da0ed567d061a7
Ajimi Mahdi <ajimimahdi1@gmail.com>
2026-05-05 20:21:28 +0200

    .

--- my-react-app/src/pages/Home.jsx
+++ my-react-app/src/pages/Home.jsx
@@ -1,13 +1,5 @@
 import MoviesCard from "../components/MoviesCard"
 
-// Creiamo un film di prova per testare il funzionamento
-const placeholderMovie = {
-  id: 1,
-  title: "Inception",
-  director: "Christopher Nolan",
-  abstract: "Un ladro che ruba segreti aziendali attraverso l'uso della tecnologia di condivisione dei sogni...",
-  image: "https://placehold.co/600x400"
-};
 
 function Home() {
   return (
@@ -16,8 +8,8 @@ function Home() {
       <h2>Benvenuto nella community degli appassionati di film</h2>
       
       <div className="row row-cols-1 row-cols-md-3 mt-4 p-5">
-        {/* Passiamo il film di prova come prop "movies" */}
-        <MoviesCard movies={placeholderMovie} />
+       
+        <MoviesCard />
       </div>
     </>
   )
